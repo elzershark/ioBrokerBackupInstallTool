651af5f34dbeecb9da149bc216f5ce9b15798c69
Elzershark <elzershark@yahoo.de>
2021-03-08 15:22:29 +0100

    Update iobroker.bat

--- InstallBackupTool/ioBrokerTool/Programm/iobroker.bat
+++ InstallBackupTool/ioBrokerTool/Programm/iobroker.bat
@@ -5,9 +5,36 @@ echo    unbedingt mit yes/ja bestaetigen.
 echo    Ist Putty dann eingelogt, kann man Putty schliessen.
 echo    Jetzt hier irgend eine Taste druecken zum installieren von iobroker.
 pause
+
+
+echo       Welche Node Version?
+echo             =============
+echo.	
+echo   [1] Node 12 (Standard)
+echo   [2] Node 14
+echo   [3] Beenden
+echo.
+set asw=0
+set /p asw="Bitte Auswahl eingeben: "
+if %asw%==1 goto NODE12
+if %asw%==2 goto NODE14
+if %asw%==3 goto STOP
+goto END
+:NODE12
 start restart.bat
 cd Programm
-start cmd /k plink.exe %BEN%@%IP% -pw %PSW% -m iobrokerinstall.txt
+start cmd /k plink.exe %BEN%@%IP% -pw %PSW% -m iobrokerinstall12.txt
 cd ..
 exit
-
+:NODE14
+start restart.bat
+cd Programm
+start cmd /k plink.exe %BEN%@%IP% -pw %PSW% -m iobrokerinstall14.txt
+cd ..
+exit
+:STOP
+start restart.bat
+exit
+:END
+start image.bat
+exit
