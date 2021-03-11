e16305105635df058a2a38d5d8c58468b0bd6de9
Elzershark <elzershark@yahoo.de>
2021-03-12 00:18:45 +0100

    Update backu.bat

--- InstallBackupTool/ioBrokerTool/Programm/backu.bat
+++ InstallBackupTool/ioBrokerTool/Programm/backu.bat
@@ -2,49 +2,37 @@
 echo                     USB- Laufwerk am Raspberry installieren
 echo                        ---------------------------------
 echo.
-echo  !!! ACHTUNG !!! Nach der Installation[1] wird der Raspi neu gestartet (sudo reboot) !!!
-echo           Meldung = FATAL ERROR: ist normal. Wegen reboot kappt die Verbindung
-echo.
 echo       !!!Der USB-Stick muss mit FAT-32 oder exFAT formatiert sein/werden !!!
 echo.
-echo   !!! Der USB-Stick muss nach jedem entfernen vom Raspy neu gemountet werden [5] !!!
+echo   !!! Der USB-Stick muss nach jedem entfernen vom Raspy neu gemountet werden [4] !!!
+echo   !!! Oder der Raspberry muss MIT USB-Stick neu gestartet werden. !!!
+echo   !!! Nach jedem Neustart des Raspberry mountet der USB Stick automatisch !!!
 echo.
-echo  !!! USB Stick kann nur gemountet werden wenn der Ordner /mount/usbstick leer ist!!!
-echo        (USB-Stick entfernen und mit Putty "dir /media/usbstick" nachschauen)
-echo.
-echo            !!! USB Backups aelter als 7 Tage werden geloescht !!!
+echo            !!! USB Backups aelter als 7 Tage werden automatisch geloescht !!!
 echo    !!! USB Backups ohne USB-Stick werden auch unter /media/usbstick gespeichert!!!
-echo   !!! Das mounten eines USB-Sticks ist dann erst wieder moeglich.... siehe oben!!!
+echo   !!! laesst der USB Stick sich nicht mounten, Raspberry mit USB-Stick neu starten!!!
 echo.
-echo			[1] USB Software installieren Starten?
-echo			[2] USB-Stick Einrichten
-echo			[3] Ich muss noch formatieren
+echo			[1] USB-Stick Einrichten
+echo			[2] Ich muss noch formatieren
 echo   -----------------------------------------------------------------
-echo			[4] USB Mount Kontrolle
-echo			[5] USB Aktivieren
-echo			[6] USB Auswerfen
-echo			[7] USB Backup erstellen
-echo			[8] Mit USB Backup ioBroker zuruecksetzen
-echo			[9] Beenden
+echo			[3] USB Mount Kontrolle
+echo			[4] USB Aktivieren
+echo			[5] USB Auswerfen
+echo			[6] USB Backup erstellen
+echo			[7] Mit USB Backup ioBroker zuruecksetzen
+echo			[8] Beenden
 echo.
 set asw=0
 set /p asw="         Bitte Auswahl eingeben: "
-if %asw%==1 goto MIT
-if %asw%==2 goto EINR
-if %asw%==3 goto AENDERN
-if %asw%==4 goto KONTROL
-if %asw%==5 goto USB
-if %asw%==6 goto USBU
-if %asw%==7 goto USBBACK
-if %asw%==8 goto USBRET
-if %asw%==9 goto BEEN
+if %asw%==1 goto EINR
+if %asw%==2 goto AENDERN
+if %asw%==3 goto KONTROL
+if %asw%==4 goto USB
+if %asw%==5 goto USBU
+if %asw%==6 goto USBBACK
+if %asw%==7 goto USBRET
+if %asw%==8 goto BEEN
 start restart.bat
-exit
-:MIT
-cd Programm
-start cmd /k plink.exe %BEN%@%IP% -pw %PSW% -m backupusb.txt
-cd ..
-start backu.bat
 exit
 :EINR
 cd Programm
@@ -100,7 +88,7 @@ set /p num="                          Bitte eintippen wie der USB-Pfad heisst. (
 set /p vat="                          Bitte exfat oder vfat eingeben: "
 echo.
 echo.
-echo crontab -l ^| sed -e '/sudo mount -t/d' ^| crontab; ^(crontab ^-l ^&^& echo "@reboot sudo mount -t %vat% -o utf8,uid=pi,gid=pi,noatime /dev/%num% /media/usbstick") ^| crontab -; sudo mount -t %vat% -o utf8,uid=pi,gid=pi,noatime /dev/%num% /media/usbstick>Programm/usb.txt
+echo crontab -l ^| sed -e '/sudo mount -t/d' ^| crontab; ^(crontab ^-l ^&^& echo "@reboot sudo mount -t %vat% -o utf8,uid=iobroker,gid=iobroker,noatime /dev/%num% /media/usbstick") ^| crontab -; sudo mount -t %vat% -o utf8,uid=iobroker,gid=iobroker,noatime /dev/%num% /media/usbstick>Programm/usb.txt
 
 start /WAIT Programm/plink.exe %BEN%@%IP% -pw %PSW% -m Programm/usb.txt
 start backu.bat
